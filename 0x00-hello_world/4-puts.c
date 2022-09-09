30a21bfcd5e54764632cc478316ad80943541472
TechieRushe <rasheedahajiferuke@gmail.com>
2022-09-09 10:10:17 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 
@@ -16,9 +15,8 @@
 int main(void)
 
 {
-    
+
     puts("\"Programming is like building a multilingual puzzle");
-    
+
     return (0);
-    
 }
