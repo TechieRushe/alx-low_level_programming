fdfd1b3c9e9d5881f6cb13a77e7da1be75b5b513
TechieRushe <rasheedahajiferuke@gmail.com>
2022-09-09 15:23:52 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Error)
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
