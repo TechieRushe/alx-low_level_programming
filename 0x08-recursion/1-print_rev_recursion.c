349e7f87f861ac40a2f1e0bed698cfa1c82318d7
TechieRushe <rasheedahajiferuke@gmail.com>
2022-09-28 13:17:02 -0700

    Print Rev Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *_print_rev_recursion - prints a string in reverse
- *@s: string to print
+ *_print_rev_recursion - prints a string, followed by a new line.
+ *@s: string to print.
+ *Return: void
  */
 
 void _print_rev_recursion(char *s);
