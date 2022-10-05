5fabf74a0c49b9726841eb4b1146288d08b6149a
TechieRushe <rasheedahajiferuke@gmail.com>
2022-10-04 17:40:39 -0700

    Count word

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,8 +4,8 @@
 
 /**
  * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- * Return: number of words
+ * @s: string
+ * Return: length
  */
 
 int count_word(char *s)
